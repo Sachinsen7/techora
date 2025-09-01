a886b5c63638bfafb7b5719e511a44130f74b9aa
Sachin Sen <sachinsen1920@gmail.com>
2025-09-01 21:36:27 +0530

    add cors setup

--- Course Selling App/index.js
+++ Course Selling App/index.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://techora-tawny.vercel.app",
     credentials: true,
   })
 );

315ccc84aaef27f2953e649d54cb1c385dde2915
Sachin Sen <sachinsen1920@gmail.com>
2025-09-02 19:50:12 +0530

    update app file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,38 @@
-import { Children, useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectIsAuthenticated, selectUser, selectAuthLoading } from './Redux/slices/authSlice';
-import ReduxInitializer from './Redux/ReduxInitializer';
-import HomePage from './pages/HomePage';
-import { PUBLIC_ROUTES, AUTH_ROUTES, PROTECTED_ROUTES } from './routes';
-import CourseListingPage from './pages/CourseListingPage';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import ForgotPassword from './pages/ForgotPassword';
-import CourseLearning from './pages/CourseLearning';
-import UserDashboard from './pages/UserDashboard';
-import UserProfile from './pages/UserProfile';
-import CheckoutPage from './pages/CheckoutPage';
-import WishlistPage from './pages/WishlistPage';
-import CartPage from './pages/CartPage';
-import HelpSupportPage from './pages/HelpSupportPage';
-import InstructorDashboard from './pages/InstructorDashboard';
-import NotFound from './pages/NotFound';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import CourseDetailsPage from './pages/CourseDetailsPage';
-import SearchBar from './components/user/SearchBar';
-import InstructorCourseFormPage from './pages/InstructorCourseFormPage';
-import InstructorCourseContentPage from './pages/InstructorCourseContentPage';
-import AdminDashboardPage from './pages/AdminDashboardPage';
-import AuthSuccess from './pages/AuthSuccess';
-import ReduxModal from './components/common/ReduxModal';
+import { Children, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  selectIsAuthenticated,
+  selectUser,
+  selectAuthLoading,
+} from "./Redux/slices/authSlice";
+import ReduxInitializer from "./Redux/ReduxInitializer";
+import HomePage from "./pages/HomePage";
+import { PUBLIC_ROUTES, AUTH_ROUTES, PROTECTED_ROUTES } from "./routes";
+import CourseListingPage from "./pages/CourseListingPage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import CourseLearning from "./pages/CourseLearning";
+import UserDashboard from "./pages/UserDashboard";
+import UserProfile from "./pages/UserProfile";
+import CheckoutPage from "./pages/CheckoutPage";
+import WishlistPage from "./pages/WishlistPage";
+import CartPage from "./pages/CartPage";
+import HelpSupportPage from "./pages/HelpSupportPage";
+import InstructorDashboard from "./pages/InstructorDashboard";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import CourseDetailsPage from "./pages/CourseDetailsPage";
+import SearchBar from "./components/user/SearchBar";
+import InstructorCourseFormPage from "./pages/InstructorCourseFormPage";
+import InstructorCourseContentPage from "./pages/InstructorCourseContentPage";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
+import AuthSuccess from "./pages/AuthSuccess";
+import ReduxModal from "./components/common/ReduxModal";
 
 const PrivateRoutes = ({ children, allowedRoles = [] }) => {
   const isAuthenticated = useSelector(selectIsAuthenticated);
@@ -61,82 +65,102 @@ function App() {
         <Navbar />
         <main className="flex-grow pt-16">
           <Routes>
-          {/* Public Routes */}
-          <Route path={PUBLIC_ROUTES.home} element={<HomePage />} />
-          <Route path={PUBLIC_ROUTES.courseListing} element={<CourseListingPage />} />
-          <Route path="/course/:id" element={<CourseDetailsPage />} /> {/* Fixed: Use string path */}
-          <Route path={PUBLIC_ROUTES.searchBar} element={<SearchBar />} />
-          <Route path={PUBLIC_ROUTES.about} element={<About />} />
-          <Route path={PUBLIC_ROUTES.contact} element={<Contact />} />
+            {/* Public Routes */}
+            <Route path={PUBLIC_ROUTES.home} element={<HomePage />} />
+            <Route
+              path={PUBLIC_ROUTES.courseListing}
+              element={<CourseListingPage />}
+            />
+            <Route path="/course/:id" element={<CourseDetailsPage />} />
+            <Route path={PUBLIC_ROUTES.searchBar} element={<SearchBar />} />
+            <Route path={PUBLIC_ROUTES.about} element={<About />} />
+            <Route path={PUBLIC_ROUTES.contact} element={<Contact />} />
 
-          {/* Auth Routes */}
-          <Route path={AUTH_ROUTES.login} element={<Login />} />
-          <Route path={AUTH_ROUTES.signup} element={<Signup />} />
-          <Route path={AUTH_ROUTES.ForgotPassword} element={<ForgotPassword />} />
-          <Route path="/auth/success" element={<AuthSuccess />} />
+            {/* Auth Routes */}
+            <Route path={AUTH_ROUTES.login} element={<Login />} />
+            <Route path={AUTH_ROUTES.signup} element={<Signup />} />
+            <Route
+              path={AUTH_ROUTES.ForgotPassword}
+              element={<ForgotPassword />}
+            />
+            <Route path="/auth/success" element={<AuthSuccess />} />
 
-          {/* Protected Routes */}
-          <Route
-            path={PROTECTED_ROUTES.dashboard}
-            element={
-              <PrivateRoutes allowedRoles={['learner', 'instructor', 'admin']}>
-                <UserDashboard />
-              </PrivateRoutes>
-            }
-          />
-          <Route
-            path="/course/:id/learning"
-            element={
-              <PrivateRoutes allowedRoles={['learner', 'instructor', 'admin']}>
-                <CourseLearning />
-              </PrivateRoutes>
-            }
-          />
-          <Route
-            path={PROTECTED_ROUTES.profile}
-            element={
-              <PrivateRoutes allowedRoles={['learner', 'instructor', 'admin']}>
-                <UserProfile />
-              </PrivateRoutes>
-            }
-          />
-          <Route
-            path={PROTECTED_ROUTES.checkout}
-            element={
-              <PrivateRoutes allowedRoles={['learner', 'instructor', 'admin']}>
-                <CheckoutPage />
-              </PrivateRoutes>
-            }
-          />
-          <Route
-            path={PROTECTED_ROUTES.wishlist}
-            element={
-              <PrivateRoutes allowedRoles={['learner', 'instructor', 'admin']}>
-                <WishlistPage />
-              </PrivateRoutes>
-            }
-          />
-          <Route
-            path={PROTECTED_ROUTES.cart}
-            element={
-              <PrivateRoutes allowedRoles={['learner', 'instructor', 'admin']}>
-                <CartPage />
-              </PrivateRoutes>
-            }
-          />
-          <Route
-            path={PROTECTED_ROUTES.helpSupport}
-            element={
-              <PrivateRoutes allowedRoles={['learner', 'instructor', 'admin']}>
-                <HelpSupportPage />
-              </PrivateRoutes>
-            }
-          />
+            {/* Protected Routes */}
+            <Route
+              path={PROTECTED_ROUTES.dashboard}
+              element={
+                <PrivateRoutes
+                  allowedRoles={["learner", "instructor", "admin"]}
+                >
+                  <UserDashboard />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path="/course/:id/learning"
+              element={
+                <PrivateRoutes
+                  allowedRoles={["learner", "instructor", "admin"]}
+                >
+                  <CourseLearning />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path={PROTECTED_ROUTES.profile}
+              element={
+                <PrivateRoutes
+                  allowedRoles={["learner", "instructor", "admin"]}
+                >
+                  <UserProfile />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path={PROTECTED_ROUTES.checkout}
+              element={
+                <PrivateRoutes
+                  allowedRoles={["learner", "instructor", "admin"]}
+                >
+                  <CheckoutPage />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path={PROTECTED_ROUTES.wishlist}
+              element={
+                <PrivateRoutes
+                  allowedRoles={["learner", "instructor", "admin"]}
+                >
+                  <WishlistPage />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path={PROTECTED_ROUTES.cart}
+              element={
+                <PrivateRoutes
+                  allowedRoles={["learner", "instructor", "admin"]}
+                >
+                  <CartPage />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path={PROTECTED_ROUTES.helpSupport}
+              element={
+                <PrivateRoutes
+                  allowedRoles={["learner", "instructor", "admin"]}
+                >
+                  <HelpSupportPage />
+                </PrivateRoutes>
+              }
+            />
             {/* Protected Routes - Instructor specific */}
             <Route
               path={PROTECTED_ROUTES.instructor}
               element={
-                <PrivateRoutes allowedRoles={['instructor', 'admin']}>
+                <PrivateRoutes allowedRoles={["instructor", "admin"]}>
                   <InstructorDashboard />
                 </PrivateRoutes>
               }
@@ -144,7 +168,7 @@ function App() {
             <Route
               path="/instructor/course/new"
               element={
-                <PrivateRoutes allowedRoles={['instructor']}>
+                <PrivateRoutes allowedRoles={["instructor"]}>
                   <InstructorCourseFormPage />
                 </PrivateRoutes>
               }
@@ -152,34 +176,32 @@ function App() {
             <Route
               path="/instructor/course/edit/:id"
               element={
-                <PrivateRoutes allowedRoles={['instructor']}>
+                <PrivateRoutes allowedRoles={["instructor"]}>
                   <InstructorCourseFormPage />
                 </PrivateRoutes>
               }
             />
             <Route
-              path="/instructor/course/:id/content" 
+              path="/instructor/course/:id/content"
               element={
-                <PrivateRoutes allowedRoles={['instructor']}>
+                <PrivateRoutes allowedRoles={["instructor"]}>
                   <InstructorCourseContentPage />
                 </PrivateRoutes>
               }
             />
 
-          
+            {/* Admin Specific Protected Routes */}
+            <Route
+              path="/admin/dashboard"
+              element={
+                <PrivateRoutes allowedRoles={["admin"]}>
+                  <AdminDashboardPage />
+                </PrivateRoutes>
+              }
+            />
 
-          {/* Admin Specific Protected Routes */}
-          <Route
-            path="/admin/dashboard"
-            element={
-              <PrivateRoutes allowedRoles={['admin']}>
-                <AdminDashboardPage />
-              </PrivateRoutes>
-            }
-          />
-
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </main>
         <Footer />
         <ReduxModal />

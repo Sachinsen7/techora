0c91f15d72e953c0438b1cfc373222728ae6e67e
Sachin Sen <sachinsen1920@gmail.com>
2025-09-02 11:13:46 +0530

    fix

--- Course Selling App/index.js
+++ Course Selling App/index.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://techora-smoky.vercel.app",
+    origin: process.env.FRONTEND_URL || "https://localhost:5173",
     credentials: true,
   })
 );

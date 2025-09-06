6a795aaead92ed6f3240587de0e2fa204d614832
Sachin Sen <sachinsen1920@gmail.com>
2025-09-06 09:41:28 +0530

    update categories

--- Frontend/src/components/course/CourseCard.jsx
+++ Frontend/src/components/course/CourseCard.jsx
@@ -71,7 +71,7 @@ function CourseCard({ course, className }) {
                   d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
                 />
               </svg>
-              {course.category.name || course.category}
+              {course.category.name || "N/A"}
             </span>
           )}
 
